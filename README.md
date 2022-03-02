e40f775e3f28233c60cbdf017ce2ab2434186d4d
dancoderegan <danielkoregan@gmail.com>
2022-03-02 11:56:22 -0330

    changes mar2

---
+++ README.md
@@ -0,0 +1,16 @@
+# Big Mary Weight Converter
+
+The only weight converter you'll ever need.
+
+This weight converter is my first foray into coding with JS, and my first project with the <a href="https://www.get-coding.ca/">Get Coding</a> program. As a newbie to the coding world, I primarily wanted to create something simple in order to build a solid foundation to grow on. I also think this project shows others a little bit about myself, mainly that I'm a little bit silly, and that I truly love Big Mary's.
+My Big Mary weight converter includes a variety of features and concepts:
+
+<ul>
+    <li> HTML based page with basic JavaScript </li>
+    <li> Number inputs & radio buttons with event listeners </li>
+    <li> Displaying output with fixed decimal results </li>
+    <li> Visual display of images matching the same number from JS output </li>
+    <li> Having the impulse control to actually weigh and photgraph a Big Mary before eating it </li>
+<ul>
+
+Big thanks to my instructor <a href="https://github.com/jackharrhy">Jack</a> for all his support. Our mutual love of chicken sandwiches, and more importantly his disdain for pickles was the driving force behind this silly little creation.
--- bigmary.js
+++ bigmary.js
@@ -5,22 +5,47 @@ Taters = 0.476198 lbs.
 Gravy = 0.308647 lbs.
 */
 
-// big mary weight //
-const bigMaryWeight = 0;
-
-function change (num) {
-  return (num / 0.405651);
+const bigMaryWeight = 0.405651;
+function change(num) {
+  return (num / bigMaryWeight).toFixed(2);
 }
-const inputElement = document.querySelector('#amount');
-const resultElement = document.querySelector('#result');
-inputElement.addEventListener('change', (event) => {
-   const result = change(parseInt(event.target.value));
-resultElement.textContent = `You weigh ${result} Big Marys`;
+const inputElement = document.querySelector("#amount");
+const resultElement = document.querySelector("#result");
+
+inputElement.addEventListener("keyup", (event) => {
+  const result = change(parseInt(event.target.value));
+  resultElement.textContent = `You weigh ${result} Big Marys`;
 });
 
-/*
-function pickleWeight (result) {
-    return (result + (result * 0.0154));
+const pickleCheck = 0.4210834;
+function addPickle(num) {
+  return (num / pickleCheck).toFixed(2);
 }
+const pickleElement = document.querySelector("#pickleYes");
+const resultPickleElement = document.querySelector("#resultPickle");
 
+pickleElement.addEventListener("click", (event) => {
+  const resultPickle = addPickle(parseInt(event.target.value));
+  resultPickleElement.textContent = `You weigh ${resultPickle} Big Marys' With Pickle`;
+});
+/*
+document.ready(function () {
+  var image = document.getElementById('bigMary').outerHTML;
+  ('#bigMary').hide();
+
+  function showInput(number) {
+    ('#result').html('');
+      for (var index = 0; index < number; index++) {
+        $('#bigMary').append(image);
+      }
+  }
+  ("#amount").keyup(function () {
+    var number = parseInt('#result').val();
+    if (isNaN(number) || (number < 1 || number > 1000)) {
+      return false;
+    }  
+    showInput(number);
+    ("#number").attr();
+});
+});
 */
